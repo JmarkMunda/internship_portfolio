7e84fe20e857f04c1237fddedb191b9f51d28539
poopijay <jaymarkmunda26@gmail.com>
2022-02-13 22:59:50 +0800

    Updated home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+import MyProfile from './Pages/MyProfile';
+import Skills from './Pages/Skills';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <HashRouter>
+      <div className='main'>
+        <Navbar /> 
+        <Routes>
+          <Route  path='/' element={<Home />} exact />
+          <Route path='/profile' element={<MyProfile />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/contact' element={<Contact />} />
+      </Routes>    
+      </div>
+    </HashRouter> 
+    
   );
 }
 
